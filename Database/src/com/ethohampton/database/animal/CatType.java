a301108fad1c77aa65d1d8d670bcdb4d2dba412d
Ethan <ethohampton@gmail.com>
2016-03-10 16:27:14 -0800

    added cats and cat types

---
+++ Database/src/com/ethohampton/database/animal/CatType.java
@@ -0,0 +1,107 @@
+package com.ethohampton.database.animal;
+
+public enum CatType {
+
+	Abyssinian, Afro_Chausie, American_Keuda, American_Shorthair, Ashera, Australian_Mist,
+
+	Aegean, American_Bobtail, American_Longhair, American_Wirehair, Asian,
+
+	African_Serval, American_Curl, American_Polydactyl, Applehead_Siamese, Asian_Semi_Longhair,
+
+	Balinese, Birman, Brazilian_Shorthair, British_Semi_Longhair,
+
+	Bengal, Blue_Russian, Bristol, British_Shorthair,
+
+	Bicolor, Bombay, British_Longhair, Burmese,
+
+	Calico, Chantilly, Tiffany, Cheetoh, Cymric,
+
+	California_Spangled_Cat, Chartreux, Colorpoint_Shorthair,
+
+	Caracat, Chausie, Cornish_Rex,
+
+	Devon_Rex, Domestic_Mediumhair,
+
+	Dilute_Calico, Domestic_Shorthaired_Cat,
+
+	Domestic_Longhaired_Cat, Don_Sphynx,
+
+	Egyptian_Mau, European_Shorthair,
+
+	Euro_Chausie, Exotic_Shorthair,
+
+	European_Burmese,
+
+	Farm_Cat,
+
+	German_Rex,
+
+	Ginger_Tabby,
+
+	Havana_Brown, Himalayan_Persian,
+
+	Hemingway,
+
+	Himalayan,
+
+	Ichabod,
+
+	Jaguarundi_Curl, Jungle_Bob,
+
+	Japanese_Bobtail, Jungle_Curl, Javanese,
+
+	Korat,
+
+	LaPerm,
+
+	Machbagral, Manx, Mixed,
+
+	Maine_Coon, Minx, Munchkin,
+
+	Maltese, Mitten_Cat,
+
+	Nebelung,
+
+	Norwegian_Forest_Cat,
+
+	Ocicat, Oriental_Bicolour, Other,
+
+	Ojos_Azules, Oriental_Longhair, Owyhee_Bob,
+
+	Oncicat, Oriental_Shorthair,
+
+	Pantherette, Pixie_bob,
+
+	Persian, Punjabi,
+
+	Peterbald,
+
+	Ragamuffin, Russian_Blue,
+
+	Ragdoll, Russian_White,
+
+	Red_Point_Siamese,
+
+	Safari, Scottish_Fold, Siamese, Singapura, Snowshoe, Sphynx,
+
+	Savannah, Selkirk_Rex, Siamese_Tabby, Skookum, Sokoke, Stone_Cougar,
+
+	Scottie_Chausie, Serengeti, Siberian, Smoke, Somali, Sumxu,
+
+	Tabby, Tiger_Siamese, Tortoiseshell, Turkish_Angora,
+
+	Thai, Tonkinese, Toyger, Turkish_Van,
+
+	Tiger_Cat, Torby, Traditional_Siamese, Tuxedo,
+
+	Ukrainian_Levkoy,
+
+	Ussuri,
+
+	Van_Kedisi,
+
+	Viverral,
+
+	York_Chocolate_Cat,
+
+}
